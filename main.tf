152f78cb9d48338672b7b6d726369c3aa0e15a8c
kgtoledoc <kgtoledoc19@gmail.com>
2022-01-10 12:09:37 -0500

    Changes in the main to deploy ec2

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ provider "aws" {
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_instance" "example-0000" {
+  ami = "ami-002068ed284fb165b"
+  shape ="t2.micro"
+  tags = {
+    Name = "Example-0000"
+    Terraform = "True"
+  }
+}
