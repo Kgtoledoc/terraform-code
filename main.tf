7ba5af6a482c29e1088e86a37284ce26f5f47cc7
Kevin Giorgio Toledo De la cruz <kevingiorgiotoledodelacruz@BOMC9081935.local>
2022-01-12 02:22:57 -0500

    aws_sg added

--- main.tf
+++ main.tf
@@ -16,9 +16,21 @@ resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_security_group" "sec-8080" {
+  name = "sg-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example-0000" {
   ami = "ami-002068ed284fb165b"
   instance_type ="t2.micro"
+  vpc_security_group_ids = [aws_security_group.sec-8080.id]
   tags = {
     Name = "Example-0000"
     Terraform = "True"
