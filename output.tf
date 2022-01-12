00a7197c88d24f39d02efea865e6db1ac5f86ce5
Kevin Toledo <kgtoledoc@unal.edu.co>
2022-01-12 02:49:22 -0500

    description in output added

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "instance_public_ip" {
     value = aws_instance.example-0000.public_ip
+    description = "The public IP of instance"
 }
 
 output "instance_private_ip" {
     value = aws_instance.example-0000.private_ip
+    description = "The private Ip of instance"
 }
